19e00ca8e21a5bc04de6e4b409347e087baac109
IamAhmedZaki <mahmedzaki670@gmail.com>
2025-09-01 21:21:35 +0500

    22

--- src/controllers/dealOfTheDayController.js
+++ src/controllers/dealOfTheDayController.js
@@ -6,9 +6,9 @@ const createDealOfTheDay = async (req, res) => {
 
     try {
 
-        const { name, endDate,products } = req.body
+        const { name, endDate, products } = req.body
         let productsData = {}
-         if (products) {
+        if (products) {
             // If it's a string, parse it
             if (typeof products === "string") {
                 productsData = JSON.parse(products);
@@ -53,4 +53,74 @@ const createDealOfTheDay = async (req, res) => {
 }
 
 
-module.exports = {createDealOfTheDay}
+
+const getDealOfTheDayByDate = async (req, res) => {
+    try {
+        const { date } = req.params;
+
+        let startOfDay=new Date(date)
+        startOfDay.setHours(0,0,0,0)
+
+        const dealProducts = await prisma.dealOfTheDay.findMany({
+            where: { startDate: {
+                gte:startOfDay
+            } },
+            include: {
+                products: {
+                    include: {
+                        Product: true
+                    }
+                }
+            }
+
+        });
+
+        if (!dealProducts) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        // Flatten category/subcategory name if needed
+        const response = 
+            dealProducts
+        ;
+
+        res.json(response);
+    } catch (error) {
+        console.error("Error fetching product:", error);
+        res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+};
+
+const getAllDealOfTheDay = async (req, res) => {
+    try {
+        
+        const dealProducts = await prisma.dealOfTheDay.findMany({
+            
+            include: {
+                products: {
+                    include: {
+                        Product: true
+                    }
+                }
+            }
+
+        });
+
+        if (!dealProducts) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        // Flatten category/subcategory name if needed
+        const response = 
+            dealProducts
+        ;
+
+        res.json(response);
+    } catch (error) {
+        console.error("Error fetching product:", error);
+        res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+};
+
+
+module.exports = { createDealOfTheDay,getDealOfTheDayByDate,getAllDealOfTheDay }
--- src/routes/dealRoutes.js
+++ src/routes/dealRoutes.js
@@ -1,10 +1,12 @@
-const { createDealOfTheDay } = require('../controllers/dealOfTheDayController');
+const { createDealOfTheDay, getDealOfTheDayByDate, getAllDealOfTheDay } = require('../controllers/dealOfTheDayController');
 const upload = require('../middlewares/uploadMiddleware');
 const express = require('express');
 const router = express.Router();
 
 
 router.post('/deal-of-the-day', createDealOfTheDay);
+router.get('/deal-of-the-day', getAllDealOfTheDay);
+router.get('/deal-of-the-day/:date', getDealOfTheDayByDate);
 
 
 module.exports = router;
